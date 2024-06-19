0dff295b4660af1fca81b4df01e90070410c9d00
Pedro Castañeda <catbox.py@gmail.com>
2024-06-18 23:29:12 -0600

    TODO

--- app/main.py
+++ app/main.py
@@ -29,8 +29,8 @@ class ImageRequest(BaseModel):
     user_profile: UserProfile
 
 
-class AudioRequest(BaseModel):
-    prompt: str
+class QuizzRequest(BaseModel):
+    content: list
 
 
 # respuesta personalizada
@@ -40,9 +40,9 @@ async def get_gpt_response(request: GPTRequest):
     system_prompt = (
         f"You are assisting a user who prefers {user_profile.learning_style} learning style and has {user_profile.disability}. "
         f"The user is interested in topics such as {', '.join(user_profile.interests)}. "
-        'Please provide the response in a structured JSON format with following format: {"content": [{"type": "paragraph","text": ""},{"type": "image","description": ""},...]}'
-        '1. Ensure the text in the "text" fields is in markdown format.'
-        '2. The "description" field of the image should provide a detailed prompt for generate the image.'
+        'Please provide the response in a structured JSON format with following format: {"content": [{"type": "paragraph","text": ""},{"type": "image","text": ""},...]}'
+        '1. Ensure the text in the "text" of the paragraph fields is in markdown format.'
+        '2. The "text" field of the image should provide a detailed prompt for generate the image.'
     )
 
     try:
@@ -84,25 +84,33 @@ async def generate_image(request: ImageRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-
-@app.post("/api/v1/generate_audio")
-async def generate_audio(request: AudioRequest):
-    audio_prompt = request.prompt
-
-    speech_file_path = Path(__file__).parent / "speech.mp3"
-    response = openai.audio.speech.create(
-        model="tts-1",
-        voice="alloy",
-        input=audio_prompt
+# quizz
+@app.post("/api/v1/generate_quizz")
+async def generate_quizz(request: QuizzRequest):
+    content = json.dumps(request.content)
+    system_prompt = (
+        f"You are going to generate 5 questions for a user. The questions are going to be generated from a reference content. That content is in the prompt."
+        'Please provide the response in a structured JSON format with following format: "[{\"question_number\": 1, \"question_text\": \"example_question_text\", \"options\": {\"example_option_1\": true, \"example_option_2\": false, \"example_option_3\": false}}, ...]"'
+        '1. Ensure the text in the fields is in the right format.'
+        '2. The first option will always be true and the last two will always be false.'
     )
 
-    response.stream_to_file(speech_file_path)
+    try:
+        response = openai.chat.completions.create(
+            model="gpt-4o",
+            response_format={"type": "json_object"},
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": content}
+            ]
+        )
 
-    url = "speech.mp3"
+        json_string = response.choices[0].message.content
+        json_object = json.loads(json_string)
 
-    response = requests.get(url)
-    with open('speech.mp3', 'wb') as file:
-        file.write(response.content)
+        return json_object
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 if __name__ == "__main__":
